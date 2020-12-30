b4ca6acd07e715eb432c0612d63bee8422b68255
Willem Monsuwe <willem@monsuwe.nl>
2020-12-30 21:52:29 +0100

    Specific script

--- scripts/init.ps1
+++ scripts/init.ps1
@@ -1,3 +1,2 @@
-Import-Module d:\app\Orchestrator\Modules\VirtualMachineManager | out-null
 $cred = new-object PSCredential('${SCVMM_USERNAME}', (ConvertTo-Securestring -force -AsPlainText -String '${SCVMM_PASSWORD}'))
 Get-SCVmmServer ${SCVMM_HOST} -Credential $cred | out-null
