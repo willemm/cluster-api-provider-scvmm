bde56cf41a5ada81496f3f49e473ef99e11851f0
Willem Monsuwe <w.monsuwe@stater.nl>
2021-02-13 23:24:13 +0100

    Compare to null in json conversion

--- scripts/functions.ps1
+++ scripts/functions.ps1
@@ -1,14 +1,14 @@
 function VMToJson($vm, $message = "") {
   $vmjson = @{}
-  if ($vm.Cloud) { $vmjson.Cloud = $vm.Cloud.Name }
-  if ($vm.Name) { $vmjson.Name = $vm.Name }
-  if ($vm.Status) { $vmjson.Status = "$($vm.Status)" }
-  if ($vm.Memory) { $vmjson.Memory = $vm.Memory }
-  if ($vm.CpuCount) { $vmjson.CpuCount = $vm.CpuCount }
-  if ($vm.VirtualNetworkAdapters) { $vmjson.CirtualNetwork = $vm.VirtualNetworkAdapters.VMNetwork.Name }
-  if ($vm.Guid) { $vmjson.Guid = $vm.Guid }
-  if ($vm.CreationTime) { $vmjson.CreationTime = $vm.CreationTime.ToString('o') }
-  if ($vm.ModifiedTime) { $vmjson.ModifiedTime = $vm.ModifiedTime.ToString('o') }
+  if ($vm.Cloud -ne $null) { $vmjson.Cloud = $vm.Cloud.Name }
+  if ($vm.Name -ne $null) { $vmjson.Name = $vm.Name }
+  if ($vm.Status -ne $null) { $vmjson.Status = "$($vm.Status)" }
+  if ($vm.Memory -ne $null) { $vmjson.Memory = $vm.Memory }
+  if ($vm.CpuCount -ne $null) { $vmjson.CpuCount = $vm.CpuCount }
+  if ($vm.VirtualNetworkAdapters -ne $null) { $vmjson.VirtualNetwork = $vm.VirtualNetworkAdapters.VMNetwork.Name }
+  if ($vm.BiosGuid -ne $null) { $vmjson.Guid = $vm.BiosGuid }
+  if ($vm.CreationTime -ne $null) { $vmjson.CreationTime = $vm.CreationTime.ToString('o') }
+  if ($vm.ModifiedTime -ne $null) { $vmjson.ModifiedTime = $vm.ModifiedTime.ToString('o') }
   if ($message) { $vmjson.Message = $message }
   $vmjson | convertto-json
 }
