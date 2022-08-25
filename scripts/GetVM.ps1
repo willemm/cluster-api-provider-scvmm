b8a9c78e2128ddebfb5123c2b95f84fab9cd428d
Willem Monsuwé <W.Monsuwe@stater.nl>
2022-08-25 11:56:08 +0200

    Return correct vm on clash or name mismatch

--- scripts/GetVM.ps1
+++ scripts/GetVM.ps1
@@ -6,6 +6,12 @@ try {
   }
   $vm = Get-SCVirtualMachine -Name $vmname
   if (-not $vm) {
+    if ($id) {
+      $vm = Get-SCVirtualMachine -ID $id
+      if ($vm) {
+	return VMToJson $vm "Not found via vmname"
+      }
+    }
     return @{ Message = "VM $($vmname) not found" } | convertto-json
   }
   $message = ""
@@ -13,19 +19,32 @@ try {
     if (-not $id) {
       throw "Fatal: More than one vm with name $vmname and no ID provided"
     }
+    # If one of the vms has an ISO coupled, it already won the race
     $realvm = $vm | Where-Object { $_.VirtualDVDDrives[0].ISO -ne $null }
     if ($realvm.Count -gt 1) {
       throw "Fatal: More than one vm with name $vmname that have an ISO coupled"
     }
     if ($realvm) {
-      $vm = $realvm | Select-Object -First 1
+      $firstvm = $realvm | Select-Object -First 1
       $message = "ISO present so this is the real vm"
     } else {
-      $vm = $vm | Sort-Object ID | Select-Object -First 1
+      # If more VMs exist, check if we have the lowest ID
+      $firstvm = $vm | Sort-Object ID | Select-Object -First 1
+      # 'may be taken' means another vm has to give up the name.
+      # But we have to wait for that because that other vm might have
+      # been alone when it checked and is now busy coupling an ISO.
       $message = "VMName may be taken"
     }
-    if ($vm.ID -ne $id) {
+    if ($firstvm.ID -ne $id) {
+      # 'is taken' means we have to give up the name
       $message = "VMName is taken"
+      # Return the correct vm
+      $vm = $vm | Where-Object { $_.ID -eq $id) | Select-Object -First 1
+      if (-not $vm) {
+	return @{ Message = "VM $($vmname) with id $($id) not found" } | convertto-json
+      }
+    } else {
+      $vm = $firstvm
     }
   }
   return VMToJson $vm $message
