ce51e42ed1808185fcea1ea09754f9f0f0d1f5eb
Willem Monsuwe <w.monsuwe@stater.nl>
2024-03-25 12:04:35 +0100

    Remove ad computer based on samaccountname, not identity
    Because of conflict records that were left hanging otherwise

--- scripts/RemoveADComputer.ps1
+++ scripts/RemoveADComputer.ps1
@@ -8,12 +8,11 @@ try {
     $dcparam.Credential = new-object PSCredential(${env:ACTIVEDIRECTORY_USERNAME}, (ConvertTo-Securestring -force -AsPlainText -String ${env:ACTIVEDIRECTORY_PASSWORD}))
   }
   $ident = "CN=$($name),$($oupath)"
-  try {
-    $comp = Get-ADComputer @dcparam -Identity $ident
-  } catch {
+  $comp = Get-ADComputer @dcparam -LDAPFilter "(samaccountname=$($name)`$)"
+  if (-not $comp) {
     return @{ Message = "ADComputer $($ident) not found" } | convertto-json
   }
-  Remove-ADComputer @dcparam -Confirm:$false -Identity $ident
+  $comp | Remove-ADComputer @dcparam -Confirm:$false
   return @{ Message = "ADComputer $($ident) removed" } | convertto-json
 } catch {
   ErrorToJson 'Remove AD Computer' $_
