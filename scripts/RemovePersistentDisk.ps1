add6a9de4cd4c5255ffbeb31834ed4a872f6574f
Willem Monsuwe <w.monsuwe@stater.nl>
2025-07-18 15:03:44 +0200

    Fix removepersistentdisk notfound handling

--- scripts/RemovePersistentDisk.ps1
+++ scripts/RemovePersistentDisk.ps1
@@ -1,13 +1,14 @@
 param($vmhost, $path, $filename)
 try {
-  $vm = New-SCVirtualMachine -VMHost $vmhost -Path $path -Name $filename
-  try {
-	  $vd = New-SCVirtualDiskDrive -UseLocalVirtualHardDisk -Path $path -FileName $filename -IDE -BUS 0 -LUN 0 VM $vm
-  } catch {
-	  if ($_.Exception.Message -match 'could not locate the specified file') {
-		  $vm = Remove-SCVirtualMachine -VM $vm
-		  return VMToJson $vm "Not Found"
-	  }
+  $vm = New-SCVirtualMachine -VMHost $vmhost -Path $path -Name "rm-$filename"
+  $err = $null
+  $vd = New-SCVirtualDiskDrive -UseLocalVirtualHardDisk -Path $path -FileName $filename -IDE -BUS 0 -LUN 0 -VM $vm -ErrorAction SilentlyContinue -ErrorVariable err
+  if ($err) {
+    $vm = Remove-SCVirtualMachine -VM $vm
+    if ($err.Exception.Message -match 'could not locate the specified file') {
+      return VMToJson $vm "Not Found"
+    }
+    return ErrorToJson 'Remove PersistentDisk' $err
   }
   $vm = Remove-SCVirtualMachine -VM $vm
   return VMToJson $vm "Removing PersistentDisk"
